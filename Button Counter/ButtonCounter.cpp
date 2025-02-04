88c2ddd19f8e7e799827b143f4c45debda0d2f33
Fredrik Blomgren <fredrik_blomgren@outlook.com>
2025-02-04 22:02:53 +0900

    Fixed input remaining in buffer && added sleep before closing

--- Button Counter/ButtonCounter.cpp
+++ Button Counter/ButtonCounter.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include<unistd.h>
+#define	microsecond 1000000
+
 std::string PrintOut(int RunTimes);
 int main() {
-	char Input;
 	int RunTimes = 0;
+	bool run = true;
+
 	std::cout << "Welcome to this hello world run counter app" << std::endl;
 	do{
 	std::cout << "Press one of the following numbers to decide what happens next. \n" 
@@ -10,8 +14,10 @@ int main() {
 	<< "	Press 2 to run the hello world counter for a set number of run times. \n"
 	<< "	Press 3 to exit the program. \n"
 	<< std::endl;	
+	std::string Input;
+
 	std::cin >> Input;
-	switch(Input){
+	switch(Input[0]){
 		case '1':
 			std::cout << PrintOut(++RunTimes) << std::endl;
 			break;
@@ -25,14 +31,17 @@ int main() {
 			break;
 		case '3':
 			std::cout << "Bye bye, You ran hello world " << RunTimes << " times. \n" << std::endl;
+			run = false;
 			break;
 		default:
 			std::cout << "Wrong key press." << std::endl;
 	}
-	
 
-	}while(Input != '3');
+	}while(run);
+	usleep(3 * microsecond);
 }
+
+
 
 std::string PrintOut(int RunTimes){
 	std::string Msg = "Hello World has been run " + std::to_string(RunTimes)+ " times! \n";
