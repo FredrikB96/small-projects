13ffb597c692f7eae74e071881967fd189ec8618
Fredrik Blomgren <fredrik_blomgren@outlook.com>
2025-02-13 22:52:59 +0900

    Modified Add&Delete for Users numbers to performe bitwise on. also updated input handling

--- Bit Manipulation Utility/BitManupalationUtil.cpp
+++ Bit Manipulation Utility/BitManupalationUtil.cpp
@@ -1,44 +1,85 @@
 #include <iostream>
+#include <vector>
 
-int ConsoleManager();
+int ConsoleManager(int input = 0);
 int InputManager();
+int IsNumber(std::string input);
+
+std::vector<int> UserNumbers;
+
 
 int main() {
-   ConsoleManager();
-   InputManager();
+    int mode = 0;
+
+    do{
+    switch (mode)
+    {
+    case 1:
+        mode = AddDeleteNumbers();
+        break;
+    case 2:
+        break;  
+    case 3:
+        break;
+    case 8: // error in program
+        break;
+    default:
+        ConsoleManager();
+        mode = InputManager();
+        break;
+    }
+    }while(mode != 4);
     return 1;
 }
 
-int ConsoleManager(){
-    std::cout << "Welcome to the Bit Manupalation Utillity! \nIt is an application to explore bitwise operators with the use of the command line!" << std::endl;
-    std::cout << "\n\n1. Enter/change numbers to bitwise\n2.Performe bitwise operator\n3.Read up on bitwise operators.\n4.exit" << std::endl;
-
+int AddDeleteNumbers(){
+    int input;
+    ConsoleManager(1);
+    input = InputManager();
     
+    if(UserNumbers.size() >input)
+    {
+        
+    }
+
+    UserNumbers.resize(input);
+
+    return 0;
+}
+
+int ConsoleManager(int input){
+    int numbers;
+    switch (input)
+    {
+        case 1:
+            std::cout << "How many numbers do you want to use: ";
+            numbers = InputManager();
+            return numbers;
+            break;   
+        default:
+            std::cout << "Welcome to the Bit Manupalation Utillity! \nIt is an application to explore bitwise operators with the use of the command line!" << std::endl;
+            std::cout << "\n\n1. Enter/change numbers to bitwise\n2.Performe bitwise operator\n3.Read up on bitwise operators.\n4.exit" << std::endl;
+            break;
+    }     
     return 1;
 }
+
+
+int IsNumber(std::string input){
+    for(int i=0; i<input.length();i++){
+        if(!isdigit(input[i]))
+            return 0;
+    }
+    return 1;
+}
+
 
 int InputManager(){
     std::string a;
     std::cin >> a;
-    switch (a[0])
-    {
-    case '1':
-        return 1;
-        break;
-    case '2':
-        return 2;
-        break;
-    case '3':
-        return 3;
-        break;
-    case '4':
-        return 4;
-        break;
-    default:
-        std::cout << a << "is not a valid input!" << std::endl;
-        return -1;
-        break;
+    if(IsNumber(a)){
+        return std::stoi(a);
     }
 
-    return 1;
+    return 0;
 }
